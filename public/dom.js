89b7b71a188febe11b12ddf49229e95b999feedf
nikkesan <nikke.san@gmail.com>
2019-11-21 15:29:03 +0000

    fixed minor errors, adjusted updateResults function to take object returned by API request, integrated into API call, and fixed the clear button navigating away from page
    closes #6
    co-authored-by: Alex <alexandra.omahony1@gmail.com>
    co-authored-by: Nikke <nikke.san@gmail.com>
    co-authored-by: Dan <dangoss@posteo.net>
    co-authored-by: Judith <jyouziel20@hotmail.com>

--- public/dom.js
+++ public/dom.js
@@ -3,22 +3,8 @@ const datalist = document.querySelector('#suggestions');
 const searchButton = document.querySelector('#searchButton');
 const clearButton = document.getElementById('clearButton');
 
-// assuming results from search.js backend api
-// now will be received in JSON format
-const testArray = [
-  'aliceblue',
-  'blue',
-  'blueviolet',
-  'cadetblue',
-  'cornflowerblue',
-  'darkblue',
-  'darkslateblue',
-  'deepskyblue'
-  // 'dodgerblue',
-  // 'lavenderblush'
-];
-
-clearButton.onclick = function clearResults() {
+clearButton.onclick = function clearResults(event) {
+  event.preventDefault();
   let defaultArray = [
     'aliceblue',
     'antiquewhite',
@@ -41,14 +27,17 @@ clearButton.onclick = function clearResults() {
   });
 }
 console.log(inputBox.value);
+
 // update option dropdown element for colors 
-function updateResults() {
+function updateResults(result) {
   // clear all current dropdown options
   while (datalist.firstChild) {
     datalist.removeChild(datalist.firstChild);
   }
+  // grab colors from passed result object
+  let colors = Object.keys(result);
   // add new dropdown options upon search
-  testArray.forEach((x, i) => {
+  colors.forEach((x, i) => {
     let colorOption = document.createElement("option");
     colorOption.classList.add("color");
     colorOption.value = x;
