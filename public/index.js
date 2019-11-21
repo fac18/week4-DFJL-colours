2105a0fa82d52625a252e9de3c8cde60d97d6e05
plasticneuron <plasticneuron@posteo.net>
2019-11-21 11:31:00 +0000

    added XHR to oninput event listener for input field
    co-authored-by: Alex <alexandra.omahony1@gmail.com>

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
-console.log('halloooo');
+console.log("halloooo");
+
+const inputField = querySelector("search-form__input");
+
+// write API in event listener
+inputField.addEventListener("input", event => {
+  let query = new XMLHttpRequest();
+  let inputText = inputField.value;
+  query.onload = () => {
+    if (query.status === 200) {
+      // DOM population function goes here
+      console.log("The DOM will now be populated with returned information");
+    } else {
+      console.log(`Error, status is: ${query.status}`);
+    }
+  };
+  query.open("GET", "/search", true);
+  query.send();
+});
